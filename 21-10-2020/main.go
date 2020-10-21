2bedf03beb25a1bad376158a92dc1046a6371096
Staford Titus S <stafordtituss@github.com>
2020-10-21 19:06:53 +0530

    Interfaces, Go Routines and Go Channels

---
+++ 21-10-2020/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// Custom logic for generating English Text
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	// Custom logic for generating Spanish Text
+	return "Hola!"
+}
